c7309ce15b601aa22adbb578397a5aad0f6e4e2a
Jack Cole <123jack.cole@gmail.com>
2020-01-10 01:23:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,9 @@ def monopoly_with_third_tier
         four: 100
       },
       names: {
-        reading_railroad: 
+        reading_railroad: ,
+        pennsylvania_railroad ,
+        
       }
 	  }
 	}
