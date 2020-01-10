bad355766947aeea931bb2c1e7c4b6f1770494dd
Jack Cole <123jack.cole@gmail.com>
2020-01-10 01:11:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,8 +2,8 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	monopoly = {
-	  boardwalk: {
-	    properties: "2"
+	  railroads: {
+	    pieces: "4"
 	  }
 	}
 	#base_hash
